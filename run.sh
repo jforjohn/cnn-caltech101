5d64d04ed15edf83eacf26d93d70bdd5a4aae091
jforjohn <giannisotiro@gmail.com>
2019-03-24 14:16:46 +0100

    RUN: extra command line args & remove sbatch partition

--- run.sh
+++ run.sh
@@ -6,12 +6,22 @@ if ! [[ $1 =~ $regex ]] ; then
   echo "error: Input should be a number > 1" >&2; exit 1
 fi
 
-for i in `seq 2 $1`;
+if ! [[ $2 =~ $regex ]] ; then
+  echo "error: Input should be a number > 1" >&2; exit 1
+fi
+
+if [[ $2 < $1 ]]; then
+    echo "error: Input should be a number > $1" >&2; exit 1
+fi
+
+for i in `seq $1 $2`;
 do
   echo $i
-  dir_prefix=model_2nd
+  dir_prefix=augment_model
+  # name of directory to save the results
   dir_name="${dir_prefix}_${i}"
   mkdir $dir_name
+  echo $dir_name
   echo
   sbatch --output=$dir_name/output.txt --error=$dir_name/error.txt sb_caltech.sh $i $dir_prefix
   # &> model_$i/output.txt
